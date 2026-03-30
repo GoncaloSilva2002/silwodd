80cbfd115798aeffbdf77436fae31e839b261371
Gonçalo Silva <goncalobarrosilva@gmail.com>
2026-03-30 18:02:28 +0100

    update para o arv2

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,12 @@
 const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || "127.0.0.1",
+  host: process.env.DB_HOST,
   port: Number(process.env.DB_PORT || 3306),
-  user: process.env.DB_USER || "root",
-  password: process.env.DB_PASSWORD || "1234",
-  database: process.env.DB_NAME || "oficina",
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: Number(process.env.DB_CONNECTION_LIMIT || 10),
   queueLimit: 0
